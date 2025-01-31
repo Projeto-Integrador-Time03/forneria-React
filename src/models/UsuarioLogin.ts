2173d7d9e8c5984f49c28c3d06d5629d77adf164
Lucas Matheus <lucas-matheusdev@outlook.com>
2025-01-31 12:32:30 -0300

    @lucas-matheus30 - Construção da lógica de autorização do login

---
+++ src/models/UsuarioLogin.ts
@@ -0,0 +1,9 @@
+export default interface UsuarioLogin {
+    id: number;
+    nome: string;
+    usuario: string;
+    senha: string;
+    endereco: string;
+    telefone: string;
+    cpf: string;
+}
