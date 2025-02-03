9eeb561c5e6e1baaf3e1f1919453c19ca65c1da2
Sarah Tavares de Lima <sarahtavarez.lima@gmail.com>
2025-02-03 10:35:18 -0300

    cardpizzas e formpizzas

---
+++ src/components/pizzas/cardpizzas/CardPizzas.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom'
+import Pizza from '../../../models/Pizza'
+
+interface CardPizzasProps {
+    pizza: Pizza
+}
+
+function CardPizza({ pizza }: CardPizzasProps) {
+    return (
+        <div className='border-slate-900 border 
+            flex flex-col rounded overflow-hidden justify-between'>
+                
+            <div>
+                <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+                    <img
+                        src={pizza.usuario?.foto}
+                        className='h-12 rounded-full'
+                        alt={pizza.usuario?.nome} />
+                    <h3 className='text-lg font-bold text-center uppercase'>
+                        {pizza.usuario?.nome}
+                    </h3>
+                </div>
+                <div className='p-4 '>
+                    <h4 className='text-lg font-semibold uppercase'>{pizza.titulo}</h4>
+                    <p>{pizza.texto}</p>
+                    <p>Tema: {pizza.tema?.descricao}</p>
+                    <p>Data: {new Intl.DateTimeFormat(undefined, {
+                        dateStyle: 'full',
+                        timeStyle: 'medium',
+                    }).format(new Date(pizza.data))}</p>
+                </div>
+            </div>
+            <div className="flex">
+                    <Link to={`/editarpizza/${pizza.id}`}
+	                className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
+                    flex items-center justify-center py-2'>
+	                <button>Editar</button>
+                </Link>
+                <Link to={`/deletarpizza/${pizza.id}`} 
+	            className='text-white bg-red-400 
+	            hover:bg-red-700 w-full flex items-center justify-center'>
+	            <button>Deletar</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default CardPizza
---
+++ src/components/pizzas/formpizzas/FormPizzas.tsx
@@ -0,0 +1,210 @@
+import { useState, useContext, useEffect, ChangeEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import Pizza from "../../../models/Pizza";
+import Tipo from "../../../models/Tipo";
+import { buscar, atualizar, cadastrar } from "../../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
+
+function FormPizza() {
+
+    const navigate = useNavigate();
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [temas, setTemas] = useState<Tema[]>([])
+
+    const [tema, setTema] = useState<Tema>({ id: 0, descricao: '', })
+    const [pizza, setPizza] = useState<Pizza>({} as Pizza)
+
+    const { id } = useParams<{ id: string }>()
+
+    const { usuario, handleLogout } = useContext(AuthContext)
+    const token = usuario.token
+
+    async function buscarPizzaPorId(id: string) {
+        try {
+            await buscar(`/postagens/${id}`, setPizza, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    async function buscarTemaPorId(id: string) {
+        try {
+            await buscar(`/temas/${id}`, setTema, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    async function buscarTemas() {
+        try {
+            await buscar('/temas', setTemas, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado');
+            navigate('/');
+        }
+    }, [token])
+
+    useEffect(() => {
+        buscarTemas()
+
+        if (id !== undefined) {
+            buscarPizzaPorId(id)
+        }
+    }, [id])
+
+    useEffect(() => {
+        setPizza({
+            ...pizza,
+            tema: tema,
+        })
+    }, [tema])
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setPizza({
+            ...pizza,
+            [e.target.name]: e.target.value,
+            tema: tema,
+            usuario: usuario,
+        });
+    }
+
+    function retornar() {
+        navigate('/postagens');
+    }
+
+    async function gerarNovaPizza(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        setIsLoading(true)
+
+        if (id !== undefined) {
+            try {
+                await atualizar(`/postagens`, pizza, setPizza, {
+                    headers: {
+                        Authorization: token,
+                    },
+                });
+
+                alert('Pizza atualizada com sucesso')
+
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout()
+                } else {
+                    alert('Erro ao atualizar a Pizza')
+                }
+            }
+
+        } else {
+            try {
+                await cadastrar(`/postagens`, pizza, setPizza, {
+                    headers: {
+                        Authorization: token,
+                    },
+                })
+
+                alert('Pizza cadastrada com sucesso');
+
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout()
+                } else {
+                    alert('Erro ao cadastrar Pizza');
+                }
+            }
+        }
+
+        setIsLoading(false)
+        retornar()
+    }
+
+    const carregandoTema = tema.descricao === '';
+
+    return (
+        <div className="container flex flex-col mx-auto items-center">
+            <h1 className="text-4xl text-center my-8">
+                {id !== undefined ? 'Editar Pizza' : 'Cadastrar Pizza'}
+            </h1>
+
+            <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovaPizza}>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="titulo">Título da Pizza</label>
+                    <input
+                        type="text"
+                        placeholder="Titulo"
+                        name="titulo"
+                        required
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={pizza.titulo}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="titulo">Texto da Pizza</label>
+                    <input
+                        type="text"
+                        placeholder="Texto"
+                        name="texto"
+                        required
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={pizza.texto}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p>Tema da Pizza</p>
+                    <select name="tema" id="tema" className='border p-2 border-slate-800 rounded'
+                        onChange={(e) => buscarTemaPorId(e.currentTarget.value)}
+                    >
+                        <option value="" selected disabled>Selecione um Tema</option>
+
+                        {temas.map((tema) => (
+                            <>
+                                <option value={tema.id} >{tema.descricao}</option>
+                            </>
+                        ))}
+
+                    </select>
+                </div>
+                <button
+                    type='submit'
+                    className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
+                               text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
+                    disabled={carregandoTema}
+                >
+                    {isLoading ?
+                        <RotatingLines
+                            strokeColor="white"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="24"
+                            visible={true}
+                        /> :
+                        <span>{id !== undefined ? 'Atualizar' : 'Cadastrar'}</span>
+                    }
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default FormPizza;
